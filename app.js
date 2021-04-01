e9682550864d5083b329b211c69ad095a37dfcfb
broganmurphy1 <57953502+broganmurphy1@users.noreply.github.com>
2021-04-01 03:00:35 +0100

    add server-side validation for client create account & client post job page

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const passport = require('passport');
 LocalStrategy = require("passport-local");
 passportLocalMongoose = require("passport-local-mongoose");
 Client = require("./models/client");
+ClientJob = require("./models/clientjob");
 const flash = require("connect-flash");
 const session = require('express-session');
 const nodeGeocoder = require('node-geocoder');
+require("dotenv").config()
 
 mongoose.connect("mongodb://localhost:27017/VolunteerDB", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('connected'))
 .catch((err)=> console.log(err));
@@ -80,30 +82,66 @@ app.get("/client-login", function(req, res){
 })
 
 app.get("/client-create-account", function(req, res){
-  res.render("client-create-account");
+  const errors = req.flash().error || [];
+  res.render("client-create-account", {errors});
 })
 
 app.post("/client-create-account", function(req, res){
-    const newClient = new Client({
-      clientFullName: req.body.clientFullName,
-      clientContactNumber: req.body.clientContactNumber,
-      clientAddressOne: req.body.clientAddressOne,
-      clientPostcode: req.body.clientPostcode,
-      clientAddressTwo: req.body.clientAddressTwo,
-      clientCity: req.body.clientCity,
-      clientMedCondition: req.body.clientMedCondition,
-      username: req.body.clientEmail,
-      password: req.body.clientPassword
-    });
 
-    newClient.save(function(err) {
-      if(err){
+  var regex = new RegExp("((\\+44(\\s\\(0\\)\\s|\\s0\\s|\\s)?)|0)7\\d{3}(\\s)?\\d{6}");
+
+  if (!req.body.clientFullName
+    || !req.body.clientContactNumber
+    || !req.body.clientAddressOne
+    || !req.body.clientPostcode
+    || !req.body.clientCity
+    || !req.body.clientEmail
+    || !req.body.clientPassword
+    || !req.body.clientConfirmPassword) {
+    req.flash('error', 'Please fill out all required fields');
+    res.redirect("client-create-account");
+  }
+  else if (!regex.test(req.body.clientContactNumber)) {
+    req.flash('error', 'Incorrect contact number, please try again');
+    res.redirect("client-create-account");
+  }
+  else if(req.body.clientPassword !== req.body.clientConfirmPassword) {
+    req.flash('error', "Passwords do not match, please try again");
+    res.redirect("client-create-account");
+  }
+  else {
+    Client.findOne({username: req.body.clientEmail}, function(err, user) {
+      if(err) {
         console.log(err);
       }
-      else{
-        res.render('client-account-success', {newUser: req.body.clientFullName});
+      if(user) {
+        req.flash('error', "Sorry, this email is already registered, please try another");
+        res.redirect("client-create-account");
       }
-    })
+      else {
+        const newClient = new Client({
+          clientFullName: req.body.clientFullName,
+          clientContactNumber: req.body.clientContactNumber,
+          clientAddressOne: req.body.clientAddressOne,
+          clientPostcode: req.body.clientPostcode,
+          clientAddressTwo: req.body.clientAddressTwo,
+          clientCity: req.body.clientCity,
+          clientMedCondition: req.body.clientMedCondition,
+          username: req.body.clientEmail,
+          password: req.body.clientPassword
+        });
+
+        newClient.save(function(err) {
+          if(err){
+            console.log(err);
+          }
+          else{
+            res.render('client-account-success', {newUser: req.body.clientFullName});
+          }
+        })
+      }
+    });
+  }
 })
 
 app.post("/client-login", passport.authenticate('local', {
@@ -111,7 +149,6 @@ app.post("/client-login", passport.authenticate('local', {
   failureRedirect: '/client-login'
 }), (req, res, next) => {
    req.session.user = req.user;
-   console.log(req.session.user);
    res.render("client-home", {user: req.user.clientFullName});
 })
 
@@ -132,15 +169,90 @@ app.get('/client-logout', function(req, res){
 });
 
 app.get("/client-post-job", ensureAuthenticated, function(req, res){
-  res.render("client-post-job");
+  const errors = req.flash().error || [];
+  res.render("client-post-job", {googlekey: process.env.GOOGLE_API_KEY, errors});
 });
 
 app.post("/client-post-job", function(req, res) {
-  console.log(req.body);
-  console.log(req.session.user);
-  geocoder.geocode(req.body.clientJobArea + ', ' + req.body.clientJobCity + ', ' + req.body.clientJobCountry  , function(err, res) {
-  console.log(res);
-});
+
+  var postcodeRegex = new RegExp("^([A-Z]{1,2}\\d[A-Z\\d]? ?\\d[A-Z]{2}|GIR ?0A{2})$")
+
+  if(req.body.clientJobPostcode && postcodeRegex.test(req.body.clientJobPostcode))
+  {
+    if(!req.body.clientJobCategory || !req.body.clientJobDesc) {
+      req.flash('error', "Please make sure all required fields are filled in and you have specified your location correctly");
+      res.redirect("client-post-job");
+    }
+    else {
+      geocoder.geocode(req.body.clientJobPostcode + ', ' + req.body.clientJobCountry, function(err, results) {
+        console.log(req.body);
+        console.log(req.session.user);
+        var lat = results[0].latitude;
+        var lon = results[0].longitude;
+
+        const newClientJob = new ClientJob({
+          clientJobCategory: req.body.clientJobCategory,
+          location: {
+            type: "Point",
+            coordinates: [parseFloat(lat), parseFloat(lon)]
+          },
+          clientJobDesc: req.body.clientJobDesc,
+          clientDetails: {
+            clientID: req.session.user._id,
+            clientName: req.session.user.clientFullName,
+            clientContactNumber: req.session.user.clientContactNumber,
+            clientMedCondition: req.session.user.clientMedCondition
+          }
+        });
+        newClientJob.save(function(err) {
+          if(err){
+            console.log(err);
+          }
+          else{
+            res.render("client-job-success");
+            console.log(newClientJob);
+          }
+        })
+      });
+    }
+  }
+  else
+  {
+    if(!req.body.clientJobCategory || !req.body.clientJobDesc || !req.body.userCurrentLat || !req.body.userCurrentLon) {
+      req.flash('error', "Please make sure all required fields are filled in and you have specified your location correctly");
+      res.redirect("client-post-job");
+    }
+    else {
+      console.log(req.body);
+      console.log(req.session.user);
+
+      var lat = req.body.userCurrentLat;
+      var lon = req.body.userCurrentLon;
+
+      const newClientJob = new ClientJob({
+        clientJobCategory: req.body.clientJobCategory,
+        location: {
+          type: "Point",
+          coordinates: [parseFloat(lat), parseFloat(lon)]
+        },
+        clientJobDesc: req.body.clientJobDesc,
+        clientDetails: {
+          clientName: req.session.user.clientFullName,
+          clientContactNumber: req.session.user.clientContactNumber,
+          clientMedCondition: req.session.user.clientMedCondition
+        }
+      });
+      newClientJob.save(function(err) {
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.render("client-job-success")
+          console.log(newClientJob);
+        }
+      })
+    }
+  }
 })
 
 app.listen(3000, function(){
--- models/clientjob.js
+++ models/clientjob.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 
-const ClientJobSchema = new mongoose.schema({
-  clientCategory: String,
-  userLat:
+const ClientJobSchema = new mongoose.Schema({
+  clientJobCategory: String,
+  location: {
+    type: {type: String},
+    coordinates: []
+  },
+  clientJobDesc: String,
+  clientDetails: {
+    clientID: String,
+    clientName: String,
+    clientContactNumber: String,
+    clientMedCondition: String
+  },
+  //timePosted:{ type : Date, default: Date.now }
 });
+
+ClientJobSchema.index({location: "2dsphere"});
+
+module.exports = mongoose.model("Clientjob", ClientJobSchema);
