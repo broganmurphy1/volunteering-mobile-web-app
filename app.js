09fec76661a45e2e221df574a925631eea85b6f0
broganmurphy1 <57953502+broganmurphy1@users.noreply.github.com>
2021-04-03 20:44:56 +0100

    ready for heroku

--- app.js
+++ app.js
@@ -536,6 +536,6 @@ app.get("/client-help-guide/delete-contact-details", ensureAuthenticated, functi
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server started on port 3000");
 })
